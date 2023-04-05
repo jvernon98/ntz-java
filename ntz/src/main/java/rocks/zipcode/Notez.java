2889024bc98f9404f5257169cb4c6ea304448a7c
Jennifer <jennifer@Zipcoders-MacBook-Pro-9.local>
2023-04-05 15:19:36 -0400

    working on it as a big ole group

--- ntz/src/main/java/rocks/zipcode/Notez.java
+++ ntz/src/main/java/rocks/zipcode/Notez.java
@@ -1,5 +1,7 @@
 package rocks.zipcode;
 
+import java.util.Set;
+
 /**
  * ntz main command.
  */
@@ -12,7 +14,7 @@ public final class Notez {
     }
     /**
      * Says hello to the world.
-     * 
+     *
      * @param args The arguments of the program.
      */
     public static void main(String argv[]) {
@@ -21,7 +23,7 @@ public final class Notez {
         if (_debug) {
             System.err.print("Argv: [");
             for (String a : argv) {
-                System.err.print(a+" ");
+                System.err.print(a + " ");
             }
             System.err.println("]");
         }
@@ -30,55 +32,116 @@ public final class Notez {
 
         ntzEngine.loadDatabase();
 
-        /*
-         * You will spend a lot of time right here.
-         * 
-         * instead of loadDemoEntries, you will implement a series
-         * of method calls that manipulate the Notez engine.
-         * See the first one:
-         */
-        ntzEngine.loadDemoEntries();
+        try {
+            switch (argv[0]) {
+                case "-r":
+                    try {
+                        ntzEngine.generalEntry(argv[1]);
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        System.err.println("Error: Remember called without note");
+                    }
+                    break;
+                case "-h":
+                    ntzEngine.help();
+                    break;
 
+                case "-c":
+                    try {
+                        ntzEngine.catergoryEntry(argv[1], argv[2]);
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        System.err.println("Error: Category called without category and/or note");
+                    }
+                    break;
+
+                case "-f":
+                    if (argv.length == 2) {
+                        ntzEngine.removeCategory(argv[1]);
+                    } else if (argv.length == 3) {
+                        ntzEngine.removeNote(argv[1], Integer.parseInt(argv[2]));
+                    } else {
+                        System.err.println("Error: Forget called with improper arguments");
+                    }
+                    break;
+
+                case "-e":
+                    try {
+                        ntzEngine.editNote(argv[1], Integer.parseInt(argv[2]), argv[3]);
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        System.err.println("Error: Edit called with improper arguments");
+                    }
+                    break;
+
+                default:
+                    System.err.printf("%s is not a valid command", argv[0]);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            ntzEngine.printResults();
+        }
         ntzEngine.saveDatabase();
 
-        if (argv.length == 0) { // there are no commandline arguments
-            //just print the contents of the filemap.
-            ntzEngine.printResults();
-        } else {
-            if (argv[0].equals("-r")) {
-                ntzEngine.addToCategory("General", argv);
-            } // this should give you an idea about how to TEST the Notez engine
-              // without having to spend lots of time messing with command line arguments.
-        }
-        /*
-         * what other method calls do you need here to implement the other commands??
-         */
-
-    }
-
-    private void addToCategory(String string, String[] argv) {
     }
 
     private void saveDatabase() {
         filemap.save();
     }
 
+    private void printResults() {
+        Set entries = filemap.keySet();
+        for(Object key : entries){
+            System.out.println(key + ":");
+            for (int i = 0; i < filemap.get(key).size(); i++){
+                System.out.printf("\t%d) %s\n", i + 1, filemap.get(key).get(i));
+            }
+        }
+    }
+    public void loadDemoEntries(){
+        filemap.put("General", new NoteList("The Very First Note"));
+        filemap.put("note2", new NoteList("A secret second note"));
+        filemap.put("category3", new NoteList("Did you buy bread AND eggs?"));
+        filemap.put("anotherNote", new NoteList("Hello from Zip Code!"));
+    }
+
+    public void catergoryEntry(String category, String note){
+        if(!filemap.containsKey(category)){
+            filemap.put(category, new NoteList(note));
+        } else{
+            filemap.get(category).add(note);
+        }
+    }
+
+    public void generalEntry(String note){
+        filemap.get("General").add(note);
+    }
+
+    public void removeCategory(String category){
+        filemap.remove(category);
+    }
+
+    public void removeNote(String catergory, int note){
+        filemap.get(catergory).remove(note - 1);
+    }
+
+    public void editNote(String category, int noteNum, String note){
+        if(filemap.containsKey(category)){
+            if(filemap.get(category).size() >= noteNum){
+                filemap.get(category).set(noteNum - 1, note);
+            } else {
+                System.err.println("Error: Note out of range");
+            }
+        } else{
+            System.err.println("Error: Category does not exist");
+        }
+    }
+
+    public void help(){
+        System.out.println("-h: prints out help info");
+        System.out.println("-r: takes a string argument and adds it to general notes");
+        System.out.println("-c: creates or appends to specified category with specific note");
+        System.out.println("-f: forgets a category by name or note by category name and number");
+        System.out.println("-e: edits a note specified by category and number");
+    }
+
     private void loadDatabase() {
         filemap.load();
     }
-
-    public void printResults() {
-        System.out.println(this.filemap.toString());
-    }
-
-    public void loadDemoEntries() {
-        filemap.put("General", new NoteList("The Very first Note"));
-        filemap.put("note2", new NoteList("A secret second note"));
-        filemap.put("category3", new NoteList("Did you buy bread AND eggs?"));
-        filemap.put("anotherNote", new NoteList("Hello from ZipCode!"));
-    }
-    /*
-     * Put all your additional methods that implement commands like forget here...
-     */
-
 }
